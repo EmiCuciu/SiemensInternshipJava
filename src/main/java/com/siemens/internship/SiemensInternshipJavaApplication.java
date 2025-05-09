c93b4ece79884879f041f2aec3e3c0b5556aeb7b
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2025-05-09 16:38:15 +0300

    test

---
+++ src/main/java/com/siemens/internship/SiemensInternshipJavaApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiemensInternshipJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiemensInternshipJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siemens/internship/SiemensInternshipJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiemensInternshipJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
